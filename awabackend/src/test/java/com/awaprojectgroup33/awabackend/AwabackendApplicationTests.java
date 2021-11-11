5a62acf70f23d9b6c2691b312e77d52e13028e63
Matias Koivumaa <matiaskoivumaa@gmail.com>
2021-11-11 12:32:35 +0200

    Spring project created

---
+++ awabackend/src/test/java/com/awaprojectgroup33/awabackend/AwabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awaprojectgroup33.awabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
